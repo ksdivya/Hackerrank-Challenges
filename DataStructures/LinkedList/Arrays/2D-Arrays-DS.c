b89e613b0f40abe780740ba61cb9c34c2ffe6a58
KS DIVYA <ksdivya16388@gmail.com>
2018-01-23 18:14:50 +0530

    Create 2D-Arrays-DS.c

---
+++ DataStructures/LinkedList/Arrays/2D-Arrays-DS.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int i,j,sum;
+    int arr[6][6],max=-36985247;
+    for(i = 0; i < 6; i++){
+       for(j = 0; j < 6; j++){
+          
+          scanf("%d",&arr[i][j]);
+       }
+    }
+     for(i=0;i<6;i++)
+     {
+         for(j=0;j<6;j++)
+         {
+             if((i+2<6) && (j+2<6))
+                 sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+             //else
+                 //return -1;
+             if(sum>max)
+                 max=sum;
+         }
+     }
+    printf("%d",max);
+    return 0;
+}
